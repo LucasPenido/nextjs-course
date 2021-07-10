77e42ceb1f474397364235726db63ff614edec2a
Lucas Penido <lpenidoa@me.com>
2021-07-09 23:35:38 -0300

    removing mongoDB variables

--- next.config.js
+++ next.config.js
@@ -1,25 +1,3 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      env: {
-        mongodb_username: 'lucas',
-        mongodb_password: 'dpYJPpHJHKIBWHm9',
-        mongodb_cluster: 'cluster0',
-        mongodb_database: 'my-site-dev',
-      },
-    };
-  }
-  
-  return {
-    reactStrictMode: true,
-    env: {
-      mongodb_username: 'lucas',
-      mongodb_password: 'dpYJPpHJHKIBWHm9',
-      mongodb_cluster: 'cluster0',
-      mongodb_database: 'my-site',
-    },
-  }
-};
+module.exports = {
+  reactStrictMode: true,
+}
